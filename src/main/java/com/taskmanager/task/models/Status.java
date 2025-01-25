6be8c694eaa7b63f5d9a586d2e4d12482cc27198
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2025-01-24 21:42:19 -0600

    Status class created and angular front end baseline

--- src/main/java/com/taskmanager/task/models/Status.java
+++ src/main/java/com/taskmanager/task/models/Status.java
@@ -1,35 +1,25 @@
 package com.taskmanager.task.models;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.time.LocalDate;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Task {
+public class Status {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
     private String description;
-    @Column(name="start_date")
-    private LocalDate startDate;
-    @Column(name = "finish_date")
-    private LocalDate finishDate;
-    @OneToOne(targetEntity = Status.class)
-    private Status status;
-    private Boolean priority;
+    @OneToOne(targetEntity = Task.class,mappedBy = "status")
+    private Task task;
 }
